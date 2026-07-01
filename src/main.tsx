3b1c1fa599cb60e8ec5b0d01ea7d3e8f20e7da9d
John Toner <johntoner404@gmail.com>
2026-07-01 23:08:01 +0100

    fix: better transition

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
 
 import { App } from "./App";
 import { Photos } from "@/pages/Photos";
 
 import "./index.css";
 
-function AnimatedRoutes() {
-  const location = useLocation();
+function AppRoutes() {
+  const { pathname } = useLocation();
   return (
-    <AnimatePresence mode="popLayout">
-      <motion.div
-        key={location.pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.18, ease: [0.4, 0, 0.2, 1] }}
-        style={{ willChange: "opacity" }}
-      >
-        <Routes location={location}>
-          <Route path="/" element={<App />} />
-          <Route path="/photos" element={<Photos />} />
-        </Routes>
-      </motion.div>
-    </AnimatePresence>
+    <div key={pathname} className="page-transition">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/photos" element={<Photos />} />
+      </Routes>
+    </div>
   );
 }
 
@@ -35,7 +25,7 @@ const loading = document.getElementById("loading");
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AnimatedRoutes />
+      <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>,
 );
